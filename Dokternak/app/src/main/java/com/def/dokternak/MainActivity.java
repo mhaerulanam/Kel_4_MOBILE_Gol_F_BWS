51751d22fadcc0e50b918104353dbb48653b82bc
Defi Tamara <defytamara2610@gmail.com>
2021-06-07 08:56:50 +0700

    Splash screen dan slider intro
    Saya sudah mencoba membuat splash screen dan slider intro pada aplikasi Dokternak. Namun warna yang digunakan masih belum fiks

---
+++ Dokternak/app/src/main/java/com/def/dokternak/MainActivity.java
@@ -0,0 +1,30 @@
+package com.def.dokternak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+                prefManager.setFirstTimeLaunch(true);
+                startActivity(new Intent(MainActivity.this, SplashScreenActivity.class));
+                finish();
+                overridePendingTransition(R.layout.fadein_splash,R.layout.fadeout_splash);
+            }
+        });
+    }
+}
