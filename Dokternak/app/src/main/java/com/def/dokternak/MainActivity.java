cf08fc9ed232348796d68a2d024317e2968f0f3f
mhaerulanam <mhaerulanam603@gmail.com>
2021-06-08 13:14:47 +0700

    Login shared preferences

--- Dokternak/app/src/main/java/com/def/dokternak/MainActivity.java
+++ Dokternak/app/src/main/java/com/def/dokternak/MainActivity.java
@@ -6,15 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    TextView nama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Deklarasi dan Menginisialisasi variable nama dengan Label Nama dari Layout MainActivity */
+        nama = findViewById(R.id.tv_namaMain);
+
+        /* Men-set Label Nama dengan data User sedang login dari Preferences */
+        nama.setText(Preferences.getLoggedInUser(getBaseContext()));
+
+        /* Men-set Status dan User yang sedang login menjadi default atau kosong di
+         * Data Preferences. Kemudian menuju ke LoginActivity*/
+        findViewById(R.id.button_logoutMain).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Menghapus Status login dan kembali ke Login Activity
+                Preferences.clearLoggedInUser(getBaseContext());
+                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                finish();
+            }
+        });
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
--- Dokternak/app/src/main/java/com/def/dokternak/WelcomeActivity.java
+++ Dokternak/app/src/main/java/com/def/dokternak/WelcomeActivity.java
@@ -120,7 +120,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private void launchHomeScreen() {
         prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
         finish();
         overridePendingTransition(R.layout.fadein_splash,R.layout.fadeout_splash);
     }
